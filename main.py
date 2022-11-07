101bcb036deaaf553e0c7ae1ac62d26cff5e7a9d
m.romanovsky@z-labs.ru <m.romanovsky@z-labs.ru>
2022-11-07 19:53:11 +0900

    progress

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("Ля ля ля")
+
+x = 50
+y = 50
+width = 40
+height = 60
+speed = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] is not False:
+        x = x - speed
+    if keys[pygame.K_RIGHT] is not False:
+        x = x + speed
+    if keys[pygame.K_UP] is not False:
+        y = y - speed
+    if keys[pygame.K_DOWN] is not False:
+        y = y + speed
+
+    win.fill((0, 0, 0))
+    pygame.draw.rect(win, (0, 0, 255), (x, y, width, height))
+    pygame.display.flip()
